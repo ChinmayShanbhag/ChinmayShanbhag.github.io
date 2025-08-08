cce254eb1281b9950ff9d585843f1479a8f94670
ChinmayShanbhag <chinmay.shanbhag@gmail.com>
2025-08-07 20:36:53 -0400

    fix: add explicit bg/text colors to body element

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-black text-black dark:text-white`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
